a7c2512e0722ff72c1413b98ec6e4d2e1a84d5ce
SamuelFM147 <115361871+SamuelFM147@users.noreply.github.com>
2024-08-21 22:25:35 -0300

    Add files via upload

--- Ex4.c
+++ Ex4.c
@@ -46,7 +46,8 @@ int main(){
 }
  
  double media(double n1, double n2){
- 	return (n1 + n2) / 2;
+ 	//return (n1 + n2) / 2;
+ 	return divisao(soma(n1,n2),2);
  }
 
  double soma(double n1, double n2){
